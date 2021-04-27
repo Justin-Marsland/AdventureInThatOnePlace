a6c2593226590d636751ffbdb1eba85b5c262f72
Justin-Marsland <j.marsland2017@gmail.com>
2021-04-27 16:10:02 -0500

    Scene Transitions
    Added scene transitions between the forest, town, and desert with a fade to black animation added in the UI HUD canvas

---
+++ AdventureInThatOnePlace/Assets/Scripts/SceneTransitions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour
+{
+    public Animator transitionAnim;
+    public string sceneName;
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if(collider.tag == "Player")
+            StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
